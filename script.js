1a76fa048e8fde47433a51c3ea7e40d9d46d57d3
Elton Lac <lacelton@yahoo.com>
2021-01-06 04:18:42 -0800

    Timer starts on button press

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-var currentQuestion = 0;
-
-var seconds = document.getElementById("countdown").textContent;
-var countdown = setInterval(function(){
+/* Countdown Timer */
+startBtn.onclick = function () {
+  var seconds = document.getElementById("countdown").textContent;
+  var countdown = setInterval(function () {
     seconds--;
-    (seconds == 1) ? document.getElementById("plural").textContent = "" : document.getElementById("plural").textContent = "s";
+    seconds == 1
+      ? (document.getElementById("plural").textContent = "")
+      : (document.getElementById("plural").textContent = "s");
     document.getElementById("countdown").textContent = seconds;
     if (seconds <= 0) clearInterval(countdown);
-},1000);
+  }, 1000);
+};
+
+var currentQuestion = 0;
